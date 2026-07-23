d5f4146d88aaf44fd4cf00659bb3e63c4cac4dc7
PSsouza10 <108752226+PSsouza10@users.noreply.github.com>
2026-07-22 21:58:25 -0300

    Adicionando RedirectView para a raiz do site

--- fitmaps_b2b/urls.py
+++ fitmaps_b2b/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.views.generic.base import RedirectView
+from gestao_treinos import views  # Certifique-se de importar as views do seu app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gestao_treinos.urls')),
+    path('', RedirectView.as_view(url='/dashboard/', permanent=False)), # Redireciona a raiz para o dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),             # Substitua 'dashboard' pela sua view real
+    path('cadastro/', views.cadastro, name='cadastro'),                # Substitua 'cadastro' pela sua view real
 ]
