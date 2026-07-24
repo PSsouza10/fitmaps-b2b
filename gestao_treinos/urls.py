67931a9aff5a3be95c19d2f22e3a344b941a4620
PSsouza10 <108752226+PSsouza10@users.noreply.github.com>
2026-07-23 23:32:36 -0300

    Atualizando urls.py com novo-treino e cadastro

--- gestao_treinos/urls.py
+++ gestao_treinos/urls.py
@@ -6,4 +6,5 @@ app_name = 'gestao_treinos'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('cadastro/', views.registrar_usuario, name='cadastro'),
+    path('novo-treino/', views.novo_treino, name='novo_treino'), # Adicione esta linha se a sua view se chamar novo_treino
 ]
