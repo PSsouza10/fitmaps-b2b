a8355c44b903395173ab545ec02035bde272f8e2
PSsouza10 <108752226+PSsouza10@users.noreply.github.com>
2026-07-23 23:13:56 -0300

    Corrigindo urls principais usando include do app gestao_treinos

--- fitmaps_b2b/urls.py
+++ fitmaps_b2b/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.views.generic.base import RedirectView
-from gestao_treinos import views  # Certifique-se de importar as views do seu app
+from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/dashboard/', permanent=False)), # Redireciona a raiz para o dashboard
-    path('dashboard/', views.dashboard, name='dashboard'),             # Substitua 'dashboard' pela sua view real
-    path('cadastro/', views.cadastro, name='cadastro'),                # Substitua 'cadastro' pela sua view real
+    path('', lambda request: HttpResponseRedirect('/dashboard/')),  # Redireciona a raiz para o dashboard
+    path('', include('gestao_treinos.urls')),                       # Delega todas as rotas para o app gestao_treinos
 ]
