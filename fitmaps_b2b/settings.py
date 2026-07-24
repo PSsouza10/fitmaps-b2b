d08439cd41f44fcc9a606d8a0c724829c4addca9
PSsouza10 <108752226+PSsouza10@users.noreply.github.com>
2026-07-24 00:12:46 -0300

    Adiciona WhiteNoise para gerenciar arquivos estaticos em producao

--- fitmaps_b2b/settings.py
+++ fitmaps_b2b/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- ADICIONE ESTA LINHA AQUI
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 ]
 
 ROOT_URLCONF = 'fitmaps_b2b.urls'
