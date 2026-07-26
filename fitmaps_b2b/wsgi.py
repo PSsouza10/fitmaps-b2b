603b06c065b7289ffa4bcaa480304f617cdf4e89
PSsouza10 <108752226+PSsouza10@users.noreply.github.com>
2026-07-25 22:43:42 -0300

    Adiciona configuracoes visuais do Jazzmin

--- fitmaps_b2b/wsgi.py
+++ fitmaps_b2b/wsgi.py
@@ -1,13 +1,15 @@
-import os
-import sys
-from pathlib import Path
+"""
+WSGI config for fitmaps_b2b project.
 
-# Garante que a raiz do projeto está no path
-root_dir = Path(__file__).resolve().parent.parent
-if str(root_dir) not in sys.path:
-    sys.path.insert(0, str(root_dir))
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitmaps_b2b.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
